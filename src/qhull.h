6be3c2a542d38a4841410795cc2a603d4329a33e
Brad Barber <bradb@shore.net>
2010-01-14 22:40:09 -0500

    Fixed quote for #include in qhull.h [U.Hergenhahn, K.Roland]

--- src/qhull.h
+++ src/qhull.h
@@ -6,13 +6,13 @@
    Proxy for libqhull.h for backwards compatability
 
    copyright (c) 1993-2010 The Geometry Center.
-   $Id: //product/qhull/main/rel/src/qhull.h#50 $$Change: 1137 $
-   $DateTime: 2010/01/02 21:58:11 $$Author: bbarber $
+   $Id: //product/qhull/main/rel/src/qhull.h#51 $$Change: 1188 $
+   $DateTime: 2010/01/14 22:35:43 $$Author: bbarber $
 */
 
 #ifndef qhDEFqhull
 #define qhDEFqhull 1
 
-#include "libqhull.h
+#include "libqhull.h"
 
 #endif /* qhDEFqhull */
